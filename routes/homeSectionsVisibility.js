d52d8089bec61208b53d5f6483d53c5cedc9a929
osama <osamaeldemerdash57@gmail.com>
2026-02-04 16:21:56 +0200

    update v2

--- routes/homeSectionsVisibility.js
+++ routes/homeSectionsVisibility.js
@@ -5,6 +5,7 @@ import { authenticateToken, requireAdmin } from '../middleware/auth.js';
 const router = express.Router();
 
 router.get('', getSettings);
+router.get('/entry', getSettings);
 router.put('', authenticateToken, requireAdmin, updateSettings);
 router.patch('/entry', authenticateToken, requireAdmin, upsertEntry);
 
